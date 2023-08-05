0e6820b96a12ec5643dc609a50225ccc99af8757
mrpassionate <rizwanalamshaon@gmail.com>
2023-08-05 20:57:01 +0600

    Adding new C++ Code

---
+++ C++ Code/address.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=5,b=6,c=10;
+    cout << "The Address of variable 'a' is " << &a << "\n";
+    cout << "The Address of variable 'b' is " << &b << "\n";
+    cout << "The Address of variable 'c' is " << &c << "\n";
+
+}
---
+++ C++ Code/pointer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=5;
+    int* pr;
+    pr=&a;
+    a=10;
+   // *pr=10; (you can also change value by this method)
+
+    cout<<"The Value of a is "<<*pr<<endl;
+    cout<<"The Value of a is "<<a<<endl;
+    return 0;
+
+}
